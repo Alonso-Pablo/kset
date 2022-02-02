73931372759abd06227fb67280cebeaded3d447b
Alonso-Pablo <pablo_nicolasalonso@yahoo.com>
2022-02-01 23:26:02 -0300

    feat: create login page

---
+++ pages/login.js
@@ -0,0 +1,13 @@
+import Layout from '../components/Layout.component'
+
+export default function Login() {
+  return (
+    <>
+      <Layout page="profile">
+        <main>
+        </main>
+      </Layout>
+    </>
+  )
+}
+
