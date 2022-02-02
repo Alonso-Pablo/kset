5bb987f54752c260380693901b0fe5f6a109cfe8
Alonso-Pablo <pablo_nicolasalonso@yahoo.com>
2022-02-01 23:26:23 -0300

    feat: create play page

---
+++ pages/play.js
@@ -0,0 +1,13 @@
+import Layout from '../components/Layout.component'
+
+export default function Play() {
+  return (
+    <>
+      <Layout page="play">
+        <main>
+        </main>
+      </Layout>
+    </>
+  )
+}
+
