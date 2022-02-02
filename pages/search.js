9b9812bf1e5ecc008d2fc2c88a32eeb4a76859dc
Alonso-Pablo <pablo_nicolasalonso@yahoo.com>
2022-02-01 23:27:30 -0300

    feat: create search page

---
+++ pages/search.js
@@ -0,0 +1,13 @@
+import Layout from '../components/Layout.component'
+
+export default function Search() {
+  return (
+    <>
+      <Layout page="search">
+        <main>
+        </main>
+      </Layout>
+    </>
+  )
+}
+
