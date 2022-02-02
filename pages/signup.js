4a6d38be06f0828858719fcb1a97b31d39fb041d
Alonso-Pablo <pablo_nicolasalonso@yahoo.com>
2022-02-01 23:28:40 -0300

    feat: create signup page

---
+++ pages/signup.js
@@ -0,0 +1,13 @@
+import Layout from '../components/Layout.component'
+
+export default function Register() {
+  return (
+    <>
+      <Layout page="profile">
+        <main>
+        </main>
+      </Layout>
+    </>
+  )
+}
+
