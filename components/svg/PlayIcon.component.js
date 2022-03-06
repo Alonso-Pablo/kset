26b394f9f1cfb2f891ee07d038e0f481ed6ae124
Alonso-Pablo <pablo_nicolasalonso@yahoo.com>
2022-03-06 17:39:41 -0300

    feat: add new icons for play page and navbar

---
+++ components/svg/PlayIcon.component.js
@@ -0,0 +1,8 @@
+export default function PlayIcon({ color='#F2EAD7' }) {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="34" height="40" viewBox="0 0 34 40" fill="none">
+      <path d="M33.5 19.134C34.1667 19.5189 34.1667 20.4811 33.5 20.866L2 39.0526C1.33333 39.4375 0.499998 38.9563 0.499998 38.1865L0.5 1.81347C0.5 1.04367 1.33333 0.562539 2 0.947439L33.5 19.134Z" fill={color} />
+    </svg>
+  )
+}
+
