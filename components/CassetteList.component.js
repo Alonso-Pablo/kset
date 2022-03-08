660010ea5314086a9a80f1f5e4a9c719926e22f3
Alonso-Pablo <pablo_nicolasalonso@yahoo.com>
2022-03-08 20:12:20 -0300

    refactor: improve name of props and variables

--- components/CassetteList.component.js
+++ components/CassetteList.component.js
@@ -1,13 +1,13 @@
 import CassetteItem from './CassetteItem.component'
 
 export default function CassetteList({
-  data,
+  cassettes,
   className,
 }) {
   return (
     <div className={className}>
       {
-        data.map(({ id, author, songName }) => <CassetteItem key={id} id={id} songName={songName} author={author} />)
+        cassettes.map(({ id, author, songName }) => <CassetteItem key={id} id={id} songName={songName} author={author} />)
       }
     </div>
   )
