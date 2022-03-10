a57173fa3737853971f14bb843ebe0530d6b7b80
Alonso-Pablo <pablo_nicolasalonso@yahoo.com>
2022-03-09 23:37:54 -0300

    refactor: convert from js to tsx and move from components/ to src/frontend/components/layout/ folder

---
+++ src/frontend/components/layout/Layout.component.tsx
@@ -0,0 +1,32 @@
+import Head from 'next/head'
+import Navbar from '../Navbar.component'
+import Footer from '../Footer.component'
+import { ReactElement } from 'react'
+
+interface LayoutProps {
+  title?: string;
+  description?: string;
+  hideSearchInput?: boolean;
+  children: ReactElement;
+}
+
+export default function Layout({
+  title = 'KSET',
+  description='Un lugar para los amantes del cassette y lo clásico.',
+  hideSearchInput = false,
+  children,
+}: LayoutProps)  {
+  return (
+    <>
+      <Head>
+        <title>{title} - KSET</title>
+        <meta name="description" content={description} />
+        <link rel="icon" href="/favicon.svg" />
+      </Head>
+
+      <Navbar hideSearchInput={hideSearchInput} />
+        { children }
+      <Footer />
+    </>
+  )
+}
