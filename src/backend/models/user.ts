ac5c9cd61db4ebf15a5d86bc5a1f2c417898e636
Alonso-Pablo <pablo_nicolasalonso@yahoo.com>
2022-03-12 23:16:50 -0300

    refactor: improve and convert file from JavaScript to TypeScript

--- src/backend/models/user.ts
+++ src/backend/models/user.ts
@@ -1,45 +1,47 @@
-const mongoose = require('mongoose')
-const bcrypt = require('bcrypt')
+import { Schema, models, model } from 'mongoose'
+import bcrypt from 'bcrypt'
+import { UserInterface } from '@shared/models/user'
 
 const SALT_WORK_FACTOR = 10
-const { Schema } = mongoose
 
-const userSchema = new Schema({
+const userSchema = new Schema<UserInterface>({
   name: {
     type: String,
     required: [true, 'Por favor ingrese su nombre de usuario'],
     minlength: [4, 'El nombre de usuario no puede contener menos de 4 caracteres'],
     maxlength: [16, 'El nombre de usuario no puede contener mas de 16 caracteres'],
     match: [/^[a-zA-Z_]{4,16}$/g, 'El formato de nombre de usuario ingresado es invalido'],
-    validate: {
-      validator: function(v){
-        return this.model('Users').findOne({ name: v }).then(user => !user)
-      },
-      message: props => `${props.value} ya esta siendo usado por otro usuario`
-    },
+    // validate: {
+    //   validator: function( name: string): any {
+    //     return this.model('Users').findOne({ name: name }).then((user: UserInterface) => !user)
+    //   },
+    //   message: props => `${props.value} ya esta siendo usado por otro usuario`
+    // },
+    trim: true,
     index: {
       unique: true
     },
-    trim: true,
+    unique: true
+
   },
   email: {
     type: String,
-    unique: 'El email ({VALUE}) ya esta siendo usado, por favor elija otro',
     required: [true, 'Por favor ingrese su email'],
     minlength: [6, "El email no puede contener menos de 6 caracteres"],
     maxlength: [60, 'El email no puede contener mas de 60 caracteres'],
     match: [/^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/, 'Formato de Email invalido'],
-    validate: {
-      validator: function(v){
-        return this.model('Users').findOne({ email: v }).then(user => !user)
-      },
-      message: props => `${props.value} ya esta siendo usado por otro usuario`
-    },
+    // validate: {
+    //   validator: function(v){
+    //     return this.model('Users').findOne({ email: v }).then(user => !user)
+    //   },
+    //   message: props => `${props.value} ya esta siendo usado por otro usuario`
+    // },
     lowercase: true,
     trim: true,
     index: {
       unique: true
-    }
+    },
+    unique: true
   },
   password: {
     type: String,
@@ -69,4 +71,4 @@ userSchema.pre('save', function(next) {
   })
 })
 
-module.exports = mongoose.models.Users || mongoose.model('Users', userSchema)
+export default models.Users || model('Users', userSchema)
